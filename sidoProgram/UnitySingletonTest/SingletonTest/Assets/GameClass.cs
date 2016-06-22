956241f5e63e1e951a1b88a1240351106c4e48db
Maxizilian <max.rosenfors@hotmail.com>
2016-06-22 12:57:31 +0200

    Skapat singletontest

---
+++ sidoProgram/UnitySingletonTest/SingletonTest/Assets/GameClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameClass : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, Manager.Instance.GetNextInt(), 0);
+        }
+    }
+}
