dddf076f94a447038ff1d925e5d4892d091bd5e7
Maxizilian <max.rosenfors@hotmail.com>
2016-07-13 12:21:51 +0200

    Fixade movecamera så att den kan vinklas

--- Pre-Alpha/Assets/Scripts/MoveCamera.cs
+++ Pre-Alpha/Assets/Scripts/MoveCamera.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveCamera : MonoBehaviour {
+public class MoveCamera : MonoBehaviour
+{
 
     GameObject Camera;
     int scrollSpeed;
-	// Use this for initialization
-	void Start () {
+    float angle;
+
+    // Use this for initialization
+    void Start()
+    {
         Camera = GameObject.Find("Main Camera");
         scrollSpeed = 1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        angle = Mathf.PI / 8;
+        Camera.transform.eulerAngles = new Vector3(90 - angle * 180 / Mathf.PI, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            Camera.transform.Translate(0, scrollSpeed, 0);
+            Camera.transform.Translate(0, scrollSpeed * Mathf.Cos(angle), scrollSpeed * Mathf.Sin(angle));
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -27,7 +34,18 @@ public class MoveCamera : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Camera.transform.Translate(0, -scrollSpeed, 0);
+            Camera.transform.Translate(0, -scrollSpeed * Mathf.Cos(angle), -scrollSpeed * Mathf.Sin(angle));
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            angle += Mathf.PI / 128;
+            Camera.transform.eulerAngles = new Vector3(90 - angle * 180 / Mathf.PI, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            angle -= Mathf.PI / 128;
+            Camera.transform.eulerAngles = new Vector3(90 - angle * 180 / Mathf.PI, 0, 0);
         }
     }
 }
